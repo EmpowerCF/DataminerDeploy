3d1d796f14a2311cae833ac098fdb536c2b0f619
b33758 <b33758@PC100681>
2023-03-28 14:24:53 +0200

    Projektdateien hinzufügen.

---
+++ EmpowerCF_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerCF_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerCF_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("1EB9C5BD-E9CF-48FE-9DC6-15C36D7C5AAA")]
+[assembly: AssemblyVersion("1.0.0.0")]
